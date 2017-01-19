a8ff73aee7e7d3e5c882bab62bbf1197268274f2
Richard Lancaster <lancasterrich@gmail.com>
2017-01-19 23:36:39 +0000

    Fix i3lock
    * Support after ImageMagik update
    * Move cursor before locking, prevents issues with unclutter

--- .i3/lock.sh
+++ .i3/lock.sh
@@ -7,9 +7,9 @@ FORTUNE_IMAGE=/tmp/fortune.png
 # Get the fortune and create an image with a transparent background
 FORTUNE="$(fortune -o -s)"
 FORTUNE="$(echo "$FORTUNE" | sed -e 's/\t/    /g' | fold -w 50 -s)"
-echo "$FORTUNE" | convert -alpha on -background '#aaa' -pointsize 50 -fill white -stroke black -strokewidth 2 label:@- -transparent '#aaa' $FORTUNE_IMAGE
+convert -alpha on -background '#aaa' -pointsize 50 -fill white -stroke black -strokewidth 2 label:"$FORTUNE" -transparent '#aaa' $FORTUNE_IMAGE
 
 scrot $IMAGE
 convert $IMAGE -blur 0x4 $IMAGE
 composite -dissolve 60% -gravity center $FORTUNE_IMAGE $IMAGE $IMAGE
-i3lock -i $IMAGE
+xte "mousemove 0 0" && i3lock -i $IMAGE
