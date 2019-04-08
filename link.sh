7bb60fe6406704db58e2dc5d6f17d791514d7ebb
Richard Lancaster <richard.lancaster@osirium.com>
2019-04-08 18:40:29 +0100

    Clean link.sh

--- link.sh
+++ link.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
+# I implore you not to use this script. 
+# Frankly, I'm stuck with it.
 shopt -s dotglob
 
-script_dir="$(dirname $(realpath $0))"
-for dir_content in "$script_dir"/*
+linkable_files_path="$(dirname $(realpath $0))"
+for target in "$linkable_files_path"/*
 do
-    file=$(basename "$dir_content")
-    location=~
-    if ! grep -qx "$file" "$script_dir/blacklist"
-    then
-        if [ -d "$dir_content" ]
-        then
-            if [ -e "$dir_content/LINK_LOCATION" ]
-            then
-                location=$(cat "$dir_content/LINK_LOCATION")
-            fi
-        elif [ -f "$dir_content" ]
-        then
-            grep_location=$(grep -oP  'LINK_LOCATION:\K(.+)' $dir_content)
-            if ! [ -z "$grep_location" ]
-            then
-                location=$grep_location
-            fi
-        fi
+    target_base=$(basename "$target")
+    name=~
 
-        link_location="$location/$file"
-        if [ -e "$link_location" ] && ! [ -L "$link_location" ]
-        then
-            echo "'$link_location' exists, creating backup"
-            mv "$link_location" "$link_location".bak
-        fi
-        ln -nfs "$(realpath "$dir_content")" "$link_location"
+    # Skip blacklisted files/dirs
+    if grep -qx "$target_base" "$linkable_files_path/blacklist"; then
+        continue
     fi
+
+    # Check for link name location file when linking a directory
+    if [ -d "$target" ]; then
+        if [ -e "$target/LINK_LOCATION" ]; then
+            name=$(cat "$target/LINK_LOCATION")
+        fi
+    # Check for link name line when linking a file
+    elif [ -f "$target" ]; then
+        grep_name=$(grep -oP  'LINK_LOCATION:\K(.+)' $target)
+        if [ -n "$grep_name" ]; then
+            name=$grep_name
+        fi
+    fi
+
+    # Backup existing files, link the file in the correct place
+    name="$name/$target_base"
+    if [ -e "$name" ] && ! [ -L "$name" ]; then
+        echo "\"$name\" exists, creating backup"
+        mv "$name" "$name".bak
+    fi
+    ln -nfs "$(realpath "$target")" "$name"
 done
 
